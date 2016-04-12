901e9c2c6553077f0837553252328e4cc85444b6
66eli77 <66eli77@gmail.com>
2016-04-12 16:32:28 -0700

    add list section, not use of any libraries.

---
+++ assets/js/kolibri_style.js
@@ -0,0 +1,7 @@
+function ko_list_selectAll(source) {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+    for (var i = 0; i < checkboxes.length; i++) {
+        if (checkboxes[i] != source)
+            checkboxes[i].checked = source.checked;
+    }
+}
